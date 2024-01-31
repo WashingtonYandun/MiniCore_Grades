656101bf7460a6660d06afc600eb446a5f987bb7
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-01-31 16:44:50 -0500

    fix: Server => Solve imports errors

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from typing import List
 from datetime import datetime
 import uvicorn
 from os import environ
-from db import get_students, get_notes, get_student
 from period import Period, PeriodRequest
 from note_controller import compute_grades
+from database import get_students
 
 
 app = FastAPI()
