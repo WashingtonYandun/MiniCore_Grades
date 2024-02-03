d60708a0555eef3dfd8644d991c23df74f12b2f0
Washington Yandun <81931984+WashingtonYandun@users.noreply.github.com>
2024-02-03 17:38:28 -0500

    docs: Improve readme.md

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -163,5 +165,5 @@ app.get('/students', (req, res) => {
 // Init server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Init server on  http://localhost:${PORT}`);
 });
